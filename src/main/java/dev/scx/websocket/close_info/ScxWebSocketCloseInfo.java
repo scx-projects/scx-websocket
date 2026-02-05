f53b032fb70acf1c6419e7a0fabaa15f3ffacb66
scx567888 <scx567888@outlook.com>
2026-02-05 16:31:44 +0800

    dev

--- src/main/java/dev/scx/websocket/close_info/ScxWebSocketCloseInfo.java
+++ src/main/java/dev/scx/websocket/close_info/ScxWebSocketCloseInfo.java
@@ -6,15 +6,23 @@ package dev.scx.websocket.close_info;
 /// @version 0.0.1
 public sealed interface ScxWebSocketCloseInfo permits ScxWebSocketCloseInfoImpl, WebSocketCloseInfo {
 
+    static ScxWebSocketCloseInfo of(int code) {
+        return new ScxWebSocketCloseInfoImpl(code, null);
+    }
+
     static ScxWebSocketCloseInfo of(int code, String reason) {
         return new ScxWebSocketCloseInfoImpl(code, reason);
+    }
+
+    static ScxWebSocketCloseInfo empty() {
+        return new ScxWebSocketCloseInfoImpl(null, null);
     }
 
     static ScxWebSocketCloseInfo ofPayload(byte[] payload) {
         return ScxWebSocketCloseInfoHelper.parseCloseInfo(payload);
     }
 
-    int code();
+    Integer code();
 
     String reason();
 
