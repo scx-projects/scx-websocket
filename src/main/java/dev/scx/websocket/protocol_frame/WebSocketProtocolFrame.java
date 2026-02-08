a7467a57343b28b15e01d4b03d8b3ea10e42ab9c
scx567888 <scx567888@outlook.com>
2026-02-08 18:30:09 +0800

    dev

--- src/main/java/dev/scx/websocket/protocol_frame/WebSocketProtocolFrame.java
+++ src/main/java/dev/scx/websocket/protocol_frame/WebSocketProtocolFrame.java
@@ -1,19 +1,17 @@
 package dev.scx.websocket.protocol_frame;
 
-import dev.scx.websocket.op_code.WebSocketOpCode;
-
-/// WebSocketFrame
+/// WebSocketProtocolFrame
 ///
 /// @author scx567888
 /// @version 0.0.1
 /// @see <a href="https://www.rfc-editor.org/rfc/rfc6455">https://www.rfc-editor.org/rfc/rfc6455</a>
-public final class WebSocketFrame {
+public final class WebSocketProtocolFrame {
 
     public boolean fin;
     public boolean rsv1;
     public boolean rsv2;
     public boolean rsv3;
-    public WebSocketOpCode opCode;
+    public int opCode;
     public boolean masked;
     public long payloadLength;
     public byte[] maskingKey;
